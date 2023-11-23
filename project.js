303898a2cb0dc65733061c7b580e136ebb3a3fd0
bryxnn <menabryan35@gmail.com>
2023-11-23 01:03:38 -0500

    lines finished

--- project.js
+++ project.js
@@ -9,7 +9,29 @@
 const prompt = require("prompt-sync")();
 
 const deposit = () => {
-    const depositAmount = prompt("Enter a deposit amount: ");
+    while (true) {
+        const depositAmount = prompt("Enter a deposit amount: ");
+        const numberDepositAmount = parseFloat(depositAmount);
+
+        if(isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+            console.log("Invalid deposit amount. Try again.")
+        } else {
+            return numberDepositAmount;
+        }
+    }
 };
 
-deposit();
+const getNumberOfLines = () => {
+    while (true) {
+        const lines = prompt("Enter the number of lines to bet on: ");
+        const numberOfLines = parseFloat(lines);
+
+        if(isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+            console.log("Invalid number of lines. Try again.")
+        } else {
+            return numberOfLines;
+        }
+    }
+}
+
+let bill = deposit();
