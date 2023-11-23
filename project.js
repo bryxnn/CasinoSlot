4c2bda0cd8e6c0ea1309efb028e06a58ae26a8f9
bryxnn <menabryan35@gmail.com>
2023-11-23 02:29:08 -0500

    finished mini project, step 2 is make a visual

--- project.js
+++ project.js
@@ -135,14 +135,31 @@ const getWinnings = (rows, bet, lines) => {
   return winnings;
 };
 
+const game = () => {
+    let balance = deposit();
+    while (true) {
+        console.log("You have a balance of $" + balance);
+        const numberOfLines = getNumberOfLines();
+        const bet = getBet(balance, numberOfLines);
+        balance -= bet * numberOfLines;
+        const reels = spin();
+        const rows = transpose(reels);
+        printRows(rows);
+        const winnings = getWinnings(rows, bet, numberOfLines);
+        balance += winnings;
+        console.log("You won, $" + winnings.toString());
+
+        if(balance <= 0) {
+            console,log("You ran out of money!");
+            break;
+        }
+
+        const playAgain = prompt("Do you want to play (y/n)?");
+
+        if (playAgain != "y") break;
+    }
+    
+}
 
 
-let balance = deposit();
-const numberOfLines = getNumberOfLines();
-const bet = getBet(balance, numberOfLines);
-const reels = spin();
-const rows = transpose(reels);
-printRows(rows);
-const winnings = getWinnings(rows, bet, numberOfLines);
-console.log("You won, $" + winnings.toString());
-
+game();
