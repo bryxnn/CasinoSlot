7fbe274b49ad3916d990687de0baae65a5538997
bryxnn <menabryan35@gmail.com>
2023-11-23 00:56:05 -0500

    finished deposit amount

--- project.js
+++ project.js
@@ -1,1 +1,15 @@
-//
+// deposit money
+// determine a number of lines
+// collect a bet
+// spin slot machine
+// check if user won
+// give the user their winnings
+// play again
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+    const depositAmount = prompt("Enter a deposit amount: ");
+};
+
+deposit();
